b14587fb9f0cb6e398d7f36d5a1213f95f529891
Bhagyam Gupta <bhagyam2000@gmail.com>
2020-06-06 15:41:56 +0530

    data_active

--- ecg_data_active.v
+++ ecg_data_active.v
@@ -1,38 +1,26 @@
 module ecg_DataActive (DataActive,ecgidx,sub_sample_info,component_idx,component_skip);
 input component_skip;
 input [1:0] ecgidx,sub_sample_info,component_idx;
+wire temp;
 output reg DataActive;
 
-always@(ecgidx,sub_sample_info,component_idx,component_skip)
-begin 
-if(component_skip)
-DataActive=1'b0;
-else if( (component_skip==0)&&(sub_sample_info==0))
-DataActive=1'b1;
-else if ( (component_skip==0)&&(sub_sample_info==1))
-begin 
-case({ecgidx,component_idx})
-4'b1001: DataActive=1'b0;
-4'b1010: DataActive=1'b0;
-4'b1101: DataActive=1'b0;
-4'b1110: DataActive=1'b0;
-default: DataActive=1'b1;
-endcase
-end
+assign temp= (component_idx==0)?0:1; // differentiate between luma and chroma component and assign a temporary varibale accordingly
 
-else if((component_skip==0)&&(sub_sample_info==2))
+always@(*)
 begin 
-case({ecgidx,component_idx})
-4'b1001: DataActive=1'b0;
-4'b1010: DataActive=1'b0;
-4'b1101: DataActive=1'b0;
-4'b1110: DataActive=1'b0;
-4'b0101: DataActive=1'b0;
-4'b0110: DataActive=1'b0;
+if(component_skip)  //checks if the component skip flag is active and makes data_active low if it is
+DataActive=1'b0;
+else
+begin
+case({ecgidx,temp,sub_sample_info})  //5 cases for which data_active is false from table 4-61 in specs.
+5'b01110: DataActive=1'b0;             
+5'b10101: DataActive=1'b0;
+5'b10110: DataActive=1'b0; 
+5'b11101: DataActive=1'b0;
+5'b11110: DataActive=1'b0;
 default: DataActive=1'b1;
 endcase
 end
 end
 endmodule
-
 
