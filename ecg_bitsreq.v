bb6ed9c07166d3a125bc01a8ccaa755065662454
Bhagyam Gupta <bhagyam2000@gmail.com>
2020-06-06 15:16:54 +0530

    bits_req

--- ecg_bitsreq.v
+++ ecg_bitsreq.v
@@ -1,130 +1,137 @@
 module Bits_required (Bits_req, sample_1,sample_2,sample_3,sample_4,ecgidx);
+parameter J = 10 ; // parameter is used for width of sample input, we need to be pass its value during module instatntiation. 
 
-parameter j = 10 ;
-
-input signed [j-1:0] sample_1,sample_2,sample_3,sample_4;
+input signed [J-1:0] sample_1,sample_2,sample_3,sample_4;
 input [1:0] ecgidx;
 output[3:0] Bits_req;
-reg [3:0] Bits_req,temp;
-wire [3:0] SM_bits_req_sample1,SM_bits_req_sample2,SM_bits_req_sample3,SM_bits_req_sample4,TC_bits_req_sample1,TC_bits_req_sample2,TC_bits_req_sample3,TC_bits_req_sample4;
 
-SM_bits_req #(j) u1(sample_1,SM_bits_req_sample1);
-SM_bits_req #(j) u2(sample_2,SM_bits_req_sample2);
-SM_bits_req #(j) u3(sample_3,SM_bits_req_sample3);
-SM_bits_req #(j) u4(sample_4,SM_bits_req_sample4);
-
-TC_bits_req #(j) m1(sample_1,TC_bits_req_sample1);
-TC_bits_req #(j) m2(sample_2,TC_bits_req_sample2);
-TC_bits_req #(j) m3(sample_3,TC_bits_req_sample3);
-TC_bits_req #(j) m4(sample_4,TC_bits_req_sample4);
+reg [3:0] Bits_req;
+wire [3:0] SM_bits_req,TC_bits_req; 
+wire [J-1:0] magnitude1,magnitude2,magnitude3,magnitude4,SM_coded_sample,converted1,converted2,converted3,converted4,TC_coded_sample;
 
 
-always@( *)
+magnitude_calculator #(J) m1(sample_1,magnitude1);  //find magnitude of each sample
+magnitude_calculator #(J) m2(sample_2,magnitude2);
+magnitude_calculator #(J) m3(sample_3,magnitude3);
+magnitude_calculator #(J) m4(sample_4,magnitude4);
+
+Convert_to_negative #(J) c1(sample_1,converted1);	//convert each positive sample to negative except for the case in which positive sample is in power of 2's for which add 1 to it and than convert to negative.
+Convert_to_negative #(J) c2(sample_2,converted2);
+Convert_to_negative #(J) c3(sample_3,converted3);
+Convert_to_negative #(J) c4(sample_4,converted4);
+
+
+assign SM_coded_sample= magnitude1|magnitude2|magnitude3|magnitude4; // taking OR of all magnitude samples gives a coded sample data for which bits_required can be directly found in SM form.
+assign TC_coded_sample= converted1&converted2&converted3&converted4; //taking AND of all negative converted samples gives a coded sample data for which bits_required can be directly found in TC form.
+
+SM_bits_req #(J) u1(SM_coded_sample,SM_bits_req);
+TC_bits_req #(J) u2(TC_coded_sample,TC_bits_req);
+
+always@(*) 
 begin
-if (ecgidx<3)
-begin
-if(SM_bits_req_sample1>SM_bits_req_sample2 && SM_bits_req_sample1>SM_bits_req_sample3 && SM_bits_req_sample1>SM_bits_req_sample4)
-Bits_req=SM_bits_req_sample1;
-else if (SM_bits_req_sample2>SM_bits_req_sample1 && SM_bits_req_sample2>SM_bits_req_sample3 && SM_bits_req_sample2>SM_bits_req_sample4)
-Bits_req=SM_bits_req_sample2;
-else if (SM_bits_req_sample3>SM_bits_req_sample1 && SM_bits_req_sample3>SM_bits_req_sample2 && SM_bits_req_sample3>SM_bits_req_sample4)
-Bits_req=SM_bits_req_sample3;
-else if (SM_bits_req_sample4>SM_bits_req_sample1 && SM_bits_req_sample4>SM_bits_req_sample2 && SM_bits_req_sample4>SM_bits_req_sample3)
-Bits_req=SM_bits_req_sample4;
-end
-
-else if (ecgidx==3)
-begin
-if(TC_bits_req_sample1>TC_bits_req_sample2 && TC_bits_req_sample1>TC_bits_req_sample3 && TC_bits_req_sample1>TC_bits_req_sample4)
-Bits_req=TC_bits_req_sample1;
-else if(TC_bits_req_sample2>TC_bits_req_sample1 && TC_bits_req_sample2>TC_bits_req_sample3 && TC_bits_req_sample2>TC_bits_req_sample4)
-Bits_req=TC_bits_req_sample2;
-else if(TC_bits_req_sample3>TC_bits_req_sample1 && TC_bits_req_sample3>TC_bits_req_sample2 && TC_bits_req_sample3>TC_bits_req_sample4)
-Bits_req=TC_bits_req_sample3;
-else if(TC_bits_req_sample4>TC_bits_req_sample1 && TC_bits_req_sample4>TC_bits_req_sample3 && TC_bits_req_sample4>TC_bits_req_sample2)
-Bits_req=TC_bits_req_sample4;
-end
+if (ecgidx==3)
+Bits_req = TC_bits_req;
+else
+Bits_req = SM_bits_req;
 end
 endmodule
 
 
 
+module SM_bits_req(sample,out);  // this module find bits_required for a positive sample input for the SM form output.
+parameter K=10;
 
-
-module SM_bits_req(sample,out);
-parameter k=10;
-
-input signed [k-1:0] sample;
+input signed [K-1:0] sample;
 output [3:0] out;
 reg [3:0] out;
 
-always@(sample)
-begin
-if (sample==0)
-out=0;
-else if(sample>=-1 && sample<= 1)
-out=1;
-else if(sample>=-3 && sample<= 3)
-out=2;
-else if(sample>=-7 && sample<= 7)
-out=3;
-else if(sample>=-15 && sample<= 15)
-out=4;
-else if(sample>=-31 && sample<= 31)
-out=5;
-else if(sample>=-63 && sample<= 63)
-out=6;
-else if(sample>=-127 && sample<= 127)
-out=7;
-else if(sample>=-255 && sample<= 255)
-out=8;
-else if(sample>=-511 && sample<= 511)
-out=9;
-else if(sample>=-1023 && sample<= 1023)
-out=10;
 
-else 
-out=15;
+always@(sample)  	//checks the occurence of 1st '1' going from MSB to LSB.
+begin
+ if(sample[K-1]==1)
+ out=K;
+ else if(sample[K-2]==1)
+ out=K-1;
+ else if(sample[K-3]==1)
+ out=K-2;
+ else if(sample[K-4]==1)
+ out=K-3;
+ else if(sample[K-5]==1)
+ out=K-4;
+ else if(sample[K-6]==1)
+ out=K-5;
+ else if(sample[K-7]==1)
+ out=K-6;
+ else if(sample[K-8]==1)
+ out=K-7;
+ else if(sample[K-9]==1)
+ out=K-8;
+ else if(K==9)
+ out=0;
+ else if(sample[K-10]==1)
+ out=K-9;
+ else
+ out=0;
 end
 endmodule
 
 
 
 
-module TC_bits_req(sample,out);
-parameter l=10;
+module TC_bits_req(sample,out);  //this module finds the bits_required for a negative sample input for TC form output.
+parameter K=10;
 
-input signed [l-1:0] sample;
+input signed [K-1:0] sample;
 output [3:0] out;
 reg [3:0] out;
 
-always@(sample)
+always@(sample)		//checks the occurence of 1st '0' going from 2nd MSB to LSB 
 begin
-if (sample==0)
-out=0;
-else if(sample>=-1 && sample<= 0)
-out=1;
-else if(sample>=-2 && sample<= 1)
-out=2;
-else if(sample>=-4 && sample<= 3)
-out=3;
-else if(sample>=-8 && sample<= 7)
-out=4;
-else if(sample>=-16 && sample<= 15)
-out=5;
-else if(sample>=-32 && sample<= 31)
-out=6;
-else if(sample>=-64 && sample<= 63)
-out=7;
-else if(sample>=-128 && sample<= 127)
-out=8;
-else if(sample>=-256 && sample<= 255)
-out=9;
-else if(sample>=-512 && sample<= 511)
-out=10;
+ if(sample[K-2]==0)
+ out=K;
+ else if(sample[K-3]==0)
+ out=K-1;
+ else if(sample[K-4]==0)
+ out=K-2;
+ else if(sample[K-5]==0)
+ out=K-3;
+ else if(sample[K-6]==0)
+ out=K-4;
+ else if(sample[K-7]==0)
+ out=K-5;
+ else if(sample[K-8]==0)
+ out=K-6;
+ else if(sample[K-9]==0)
+ out=K-7;
+ else if(K==9)
+ out=1;
+ else if(sample[K-10]==0)
+ out=K-8;
+ else
+ out=1;
+end
 
-else 
-out=15; //indicates problem
+endmodule
+
+
+
+
+
+
+module magnitude_calculator(sample,magnitude); // This module gives magnitude of a signed number 
+
+parameter K = 10 ;
+
+input signed [K-1:0] sample;
+output [K-1:0] magnitude;
+reg  [K-1:0] magnitude;
+
+always@(*)
+begin
+if(sample[K-1]==1)             //check the msb of sample
+magnitude=~sample+1'b1;       // if negative find its magnitude
+else
+magnitude=sample;
 end
 endmodule
 
@@ -134,47 +141,29 @@ endmodule
 
 
 
-// magnitude_calculator #(j)m1(sample_1,mag1);
-// magnitude_calculator #(j)m2(sample_2,mag2);
-// magnitude_calculator #(j)m3(sample_3,mag3);
-// magnitude_calculator #(j)m4(sample_4,mag4);
+module Convert_to_negative(sample,converted); // This module makes positive number negative except the positive number which is in power of twos for which it adds 1 to it and than gives negative of it. 
 
+parameter K = 10 ;
 
+input signed [K-1:0] sample;
+output [K-1:0] converted;
+reg  [K-1:0] converted,temp;
 
-
-
-
-
-
-
-
-
-
-
-// module magnitude_calculator(sample,magnitude,sign);
-
-// parameter k = 10 ;
-
-// input signed [k-1:0] sample;
-// output [k-1:0] magnitude;
-// output sign;
-// reg  [k-1:0] magnitude;
-// reg [k-1:0] temp;
-
-// always@(sample)
-// begin
-// if(sample<0)
-// begin
-// temp=~sample;
-// magnitude=temp+1;
-// sign=1;
-// end
-// else
-// begin
-// magnitude=sample;
-// sign=0;
-// end
-// endmodule
-
+always@(*)
+begin 
+if(sample[K-1]==0) // checks if sample is positive
+begin
+ if(((sample)&(sample-1))==0)   //Logic to check if the positive sample is in power of 2's.
+ begin
+ temp=sample+1;   // if in power of 2's add 1 and then make negative. It helps in calculating correct bits_required.
+ converted= ~temp +1 ; // converts positive to negative in 2's compliment representation.
+ end
+ else
+ converted= ~sample +1 ;  // converts positive to negative in 2's compliment representation.
+end
+else
+converted=sample; // if already negative donot convert.
+end
+endmodule
 
 
